37caa36179cdd275cda604a62fcf7da29578611f
Dan Picton <daniel.picton@gmail.com>
2020-02-22 07:45:31 +0000

    Add basic controller logic

---
+++ mid_logic.py
@@ -0,0 +1,24 @@
+import db_model
+import time
+
+
+def get_epoch():
+    return int(time.time())
+
+
+class JournalSevrice:
+    def __init__(self):
+        self.model = db_model.DbConnection()
+
+    def new_entry(self, log_entry, log_stamp=get_epoch(), source="dev", log_type="none"):
+        self.model.make_log_entry(log_stamp, source, log_type, log_entry)
+
+    def log_range(self, low_stamp=get_epoch() - 604800, high_stamp=get_epoch()):
+        return self.model.get_log_range(low_stamp, high_stamp)
+
+
+a = JournalSevrice()
+a.new_entry("blah2", 131, "blah3", "blah4")
+a.new_entry("boeulah2", 134, "bouelah3", "bloeuah4")
+a.new_entry( "blah2", 138, "blah3", "blah4")
+print(a.log_range(120, 140))
